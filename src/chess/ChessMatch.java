cf2dd633a53aa57d31c2d705e0936b147c342cae
Tiago Pimenta <tiago.pimenta@ucsal.edu.br>
2024-07-05 17:17:23 -0300

    Pequena melhoria na impressão do tabuleiro!

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -31,9 +31,20 @@ public class ChessMatch {
 	}
 	
 	private void inicialSetup() {
-		placeNewPiece('b', 6, new Rook(board, Color.WHITHE));
-		placeNewPiece('e', 8, new King(board, Color.BLACK));
-		placeNewPiece('e', 1, new King(board, Color.WHITHE));
+		
+		 placeNewPiece('c', 1, new Rook(board, Color.WHITHE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITHE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITHE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITHE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITHE));
+        placeNewPiece('d', 1, new King(board, Color.WHITHE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
 		
 	}
 
