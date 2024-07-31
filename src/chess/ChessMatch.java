6dbed2fe0e0c57e97f9786808439b9b606250a35
TiagoEngBrazil <tiago.pimenta@ucsal.edu.br>
2024-07-31 20:12:31 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -118,7 +118,10 @@ public class ChessMatch {
 	}
 
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		
+		p.increaseMovrCount();
+		
 		Piece capturedPiece = board.removePiece(target);
 
 		board.placePiece(p, target);
@@ -133,7 +136,10 @@ public class ChessMatch {
 	}
 
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		
+		p.decreaseMovrCount();
+		
 		board.placePiece(p, source);
 
 		if (capturedPiece != null) {
